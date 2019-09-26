b8ffdfde1bf2861d81731b943c39f6d28b5525da
Leon Zimmermann <leon.zimmermann@hotmail.com>
2019-09-26 18:21:25 +0200

    Added conversion method to measures

--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/models/measure/Measure.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/models/measure/Measure.kt
@@ -2,13 +2,17 @@ package com.hotmail.leon.zimmermann.homeassistant.models.measure
 
 enum class Measure(
     val text: String,
-    val abbreviation: String
+    val abbreviation: String,
+    private val baseFactor: Double
 ) {
-    MILLIGRAM("milligram", "mg"),
-    GRAM("gram", "g"),
-    KILOGRAM("kilogram", "kg"),
-    MILILITER("milliliter", "ml"),
-    CENTILITER("centiliter", "cl"),
-    DECILITER("deciliter", "dl"),
-    LITER("liter", "l")
+    MILLIGRAM("milligram", "mg", 0.001),
+    GRAM("gram", "g", 1.0),
+    KILOGRAM("kilogram", "kg", 1000.0),
+    MILILITER("milliliter", "ml", 0.001),
+    CENTILITER("centiliter", "cl", 0.01),
+    DECILITER("deciliter", "dl", 0.1),
+    LITER("liter", "l", 1.0);
+
+    fun toBaseMeasure(value: Double) = value * baseFactor
+    fun fromBaseMeasure(value: Double) = value / baseFactor
 }
