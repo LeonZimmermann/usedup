1e92a981475fd5fa3b2f490f8a5c363c5962e388
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-05-15 17:29:34 +0200

    Added EmailRegistrationFragment

---
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/ui/fragments/register/EmailRegistrationViewModel.kt
@@ -0,0 +1,7 @@
+package com.hotmail.leon.zimmermann.homeassistant.ui.fragments.register
+
+import androidx.lifecycle.ViewModel
+
+class EmailRegistrationViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
