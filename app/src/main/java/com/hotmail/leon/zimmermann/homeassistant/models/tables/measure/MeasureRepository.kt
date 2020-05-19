eba87ba30abb69eec4c68722515afb834173f8bb
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-05-19 14:15:40 +0200

    Deleted old datamodel

--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/models/tables/measure/MeasureRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.hotmail.leon.zimmermann.homeassistant.models.tables.measure
-
-class MeasureRepository(private val measureDao: MeasureDao) {
-    val measureEntityList = measureDao.getAll()
-
-    suspend fun insert(measureEntity: MeasureEntity) {
-        measureDao.insert(measureEntity)
-    }
-}
