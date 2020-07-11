d1e35423ac2cea9f914981100f05b5ccb94d90ec
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-07-11 22:24:35 +0200

    Added navigation drawer

--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/ui/activities/AppActivity.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/ui/activities/AppActivity.kt
@@ -1,13 +1,16 @@
 package com.hotmail.leon.zimmermann.homeassistant.ui.activities
 
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.hotmail.leon.zimmermann.homeassistant.R
+import kotlinx.android.synthetic.main.app_activity.*
 
 class AppActivity: AppCompatActivity() {
     private lateinit var navController: NavController
@@ -21,8 +24,9 @@ class AppActivity: AppCompatActivity() {
 
     private fun initNavigation() {
         navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawer)
         setupActionBarWithNavController(navController, appBarConfiguration)
+        navigation_view.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
