98f928da30215830dd123b864d4a04fc91a6e48d
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-10-06 18:51:00 +0200

    Refactored overview view

--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/overview/OverviewFragment.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/overview/OverviewFragment.kt
@@ -1,60 +1,57 @@
 package com.hotmail.leon.zimmermann.homeassistant.app.overview
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.hotmail.leon.zimmermann.homeassistant.R
+import com.hotmail.leon.zimmermann.homeassistant.components.SimpleProductPreviewAdapter
+import com.hotmail.leon.zimmermann.homeassistant.databinding.OverviewFragmentBinding
 import kotlinx.android.synthetic.main.overview_fragment.*
 
 class OverviewFragment : Fragment() {
 
-    private lateinit var viewModel: OverviewViewModel
+  private val viewModel: OverviewViewModel by viewModels()
+  private lateinit var binding: OverviewFragmentBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
-        initViewModel()
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setHasOptionsMenu(true)
+  }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.overview_fragment, container, false)
-    }
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.overview_fragment, container, false)
+  }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initDiscrepancyCard()
-    }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initDatabinding(view)
+    initDiscrepancyCard()
+  }
 
-    private fun initViewModel() {
-        viewModel = ViewModelProviders.of(this).get(OverviewViewModel::class.java)
-    }
+  private fun initDatabinding(view: View) {
+    binding = OverviewFragmentBinding.bind(view)
+    binding.viewModel = viewModel
+    binding.lifecycleOwner = this
+  }
 
-    private fun initDiscrepancyCard() {
-        val adapter = com.hotmail.leon.zimmermann.homeassistant.components.SimpleProductPreviewAdapter(
-            context!!,
-            discrepancy_recyclerview
-        )
-        discrepancy_recyclerview.adapter = adapter
-        discrepancy_recyclerview.layoutManager = LinearLayoutManager(context!!)
-        viewModel.discrepancyProductList.observe(viewLifecycleOwner, Observer { discrepancyProductList ->
-            adapter.productAmountList = discrepancyProductList
-        })
-        viewModel.discrepancyAdditionalAmount.observe(viewLifecycleOwner, Observer { discrepancyAdditionalAmount ->
-            adapter.additionalAmount = discrepancyAdditionalAmount
-        })
-        shopping_button.setOnClickListener {
-            findNavController().navigate(R.id.action_global_shopping_fragment)
-        }
-    }
+  private fun initDiscrepancyCard() {
+    val adapter = SimpleProductPreviewAdapter(requireContext(), discrepancy_recyclerview)
+    discrepancy_recyclerview.adapter = adapter
+    discrepancy_recyclerview.layoutManager = LinearLayoutManager(requireContext())
+    viewModel.discrepancyProductList.observe(viewLifecycleOwner, Observer { discrepancyProductList ->
+      adapter.productAmountList = discrepancyProductList
+    })
+    viewModel.discrepancyAdditionalAmount.observe(viewLifecycleOwner, Observer { discrepancyAdditionalAmount ->
+      adapter.additionalAmount = discrepancyAdditionalAmount
+    })
+  }
 
-    companion object {
-        fun newInstance() = OverviewFragment()
-    }
+  companion object {
+    fun newInstance() = OverviewFragment()
+  }
 }
--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/overview/OverviewViewModel.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/overview/OverviewViewModel.kt
@@ -1,10 +1,13 @@
 package com.hotmail.leon.zimmermann.homeassistant.app.overview
 
 import android.app.Application
+import android.view.View
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import androidx.navigation.Navigation
+import com.hotmail.leon.zimmermann.homeassistant.R
 import com.hotmail.leon.zimmermann.homeassistant.datamodel.objects.Product
 import com.hotmail.leon.zimmermann.homeassistant.datamodel.repositories.product.ProductRepository
 import kotlin.math.max
@@ -20,6 +23,10 @@ class OverviewViewModel(application: Application) : AndroidViewModel(application
 
   val discrepancyAdditionalAmount: LiveData<Int> = Transformations.map(products) { products ->
     max(0, products.filter { it.discrepancy > 0 }.size - DISCREPANCY_LIST_SIZE)
+  }
+
+  fun onShoppingButtonClicked(view: View) {
+    Navigation.findNavController(view).navigate(R.id.action_global_shopping_fragment)
   }
 
   companion object {
