2f47f7619575502b42bf87318a777430d6a77c9e
Leon Zimmermann <leon.zimmermann@hotmail.com>
2020-10-08 22:05:41 +0200

    Implemented databinding in account view

--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/account/AccountFragment.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/account/AccountFragment.kt
@@ -1,57 +1,45 @@
 package com.hotmail.leon.zimmermann.homeassistant.app.account
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
-
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import com.google.android.material.snackbar.Snackbar
 import com.hotmail.leon.zimmermann.homeassistant.R
+import com.hotmail.leon.zimmermann.homeassistant.databinding.AccountFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.account_fragment.*
 
 @AndroidEntryPoint
 class AccountFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = AccountFragment()
-    }
+  private val viewModel: AccountViewModel by viewModels()
 
-    private lateinit var viewModel: AccountViewModel
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.account_fragment, container, false)
+  }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.account_fragment, container, false)
-    }
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initDatabinding()
+    initErrorMessage()
+  }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(AccountViewModel::class.java)
-        val authentication = FirebaseAuth.getInstance()
-        authentication.currentUser?.let { user ->
-            user.photoUrl?.let {
-                Glide.with(this)
-                    .load(it)
-                    .circleCrop()
-                    .into(profile_image)
-            }
-            user.displayName?.let { name_input.setText(it) }
-            user.email?.let { email_input.setText(it) }
-        } ?: run {
-            Toast.makeText(context, "Cannot access account information", Toast.LENGTH_LONG).show()
-            findNavController().navigateUp()
-        }
-        logout_button.setOnClickListener {
-            authentication.signOut()
-            // TODO Navigate out of app
-        }
-    }
+  private fun initDatabinding() {
+    val binding = AccountFragmentBinding.bind(requireView())
+    binding.lifecycleOwner = this
+    binding.viewModel = viewModel
+  }
 
+  private fun initErrorMessage() {
+    viewModel.errorMessage.observe(viewLifecycleOwner, Observer { errorMessage ->
+      Snackbar.make(requireView(), errorMessage, Snackbar.LENGTH_LONG).show()
+    })
+  }
+
+  companion object {
+    fun newInstance() = AccountFragment()
+  }
 }
--- app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/account/AccountViewModel.kt
+++ app/src/main/java/com/hotmail/leon/zimmermann/homeassistant/app/account/AccountViewModel.kt
@@ -1,7 +1,48 @@
 package com.hotmail.leon.zimmermann.homeassistant.app.account
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+  private val authentication = FirebaseAuth.getInstance()
+
+  val errorMessage: MutableLiveData<String> = MutableLiveData()
+  val userImage: MutableLiveData<String> = MutableLiveData()
+  val userName: MutableLiveData<String> = MutableLiveData()
+  val userEmail: MutableLiveData<String> = MutableLiveData()
+
+  init {
+    try {
+      val user = requireNotNull(authentication.currentUser)
+      userImage.postValue(user.photoUrl?.toString())
+      userName.postValue(user.displayName)
+      userEmail.postValue(user.email)
+    } catch (e: IllegalArgumentException) {
+      errorMessage.postValue("Cannot access account information")
+      // TODO Navigate up
+    }
+  }
+
+  fun onLogoutClicked() {
+    authentication.signOut()
+    // TODO Navigate out of app
+  }
+
+  companion object {
+    @JvmStatic
+    @BindingAdapter("imageUrl")
+    fun loadImage(view: ImageView, url: String?) {
+      url?.let {
+        Glide.with(view.context)
+          .load(it)
+          .circleCrop()
+          .into(view)
+      }
+    }
+  }
 }
