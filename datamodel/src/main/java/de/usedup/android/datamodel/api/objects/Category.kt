da3ce95e6598ca1e0c273a249119e8f54c721ffb
Leon Zimmermann <leon.zimmermann@hotmail.com>
2021-04-23 02:38:05 +0200

    added translations for categories

--- datamodel/src/main/java/de/usedup/android/datamodel/api/objects/Category.kt
+++ datamodel/src/main/java/de/usedup/android/datamodel/api/objects/Category.kt
@@ -3,12 +3,22 @@ package de.usedup.android.datamodel.api.objects
 import de.usedup.android.datamodel.api.exceptions.DataIntegrityException
 import de.usedup.android.datamodel.firebase.objects.FirebaseCategory
 import de.usedup.android.datamodel.firebase.objects.FirebaseId
+import java.util.*
 
 class Category(
     val id: Id,
-    var name: String,
+    private val _name: Map<String, String>,
     var position: Int
 ) {
+
+    /**
+     * The names for categories are stored in maps, with a language as the key and the according string as the value.
+     * This enables the storage of category names in multiple languages. _name is the said map and the name field returns
+     * the translation for the current Locale, or the english translation as a fallback.
+     */
+    val name: String
+        get() = _name[Locale.getDefault().language] ?: requireNotNull(_name["en"])
+
     companion object {
         internal fun createInstance(id: String, firebaseObject: FirebaseCategory): Category {
             val name = firebaseObject.name ?: throw DataIntegrityException()
--- datamodel/src/main/java/de/usedup/android/datamodel/firebase/objects/FirebaseCategory.kt
+++ datamodel/src/main/java/de/usedup/android/datamodel/firebase/objects/FirebaseCategory.kt
@@ -3,8 +3,8 @@ package de.usedup.android.datamodel.firebase.objects
 import com.google.firebase.firestore.PropertyName
 
 internal data class FirebaseCategory(
-    @PropertyName("name") var name: String? = null,
-    @PropertyName("position") var position: Int? = null
+  @PropertyName("name") var name: Map<String, String>? = null,
+  @PropertyName("position") var position: Int? = null
 ) {
   companion object {
     const val COLLECTION_NAME = "categories"
